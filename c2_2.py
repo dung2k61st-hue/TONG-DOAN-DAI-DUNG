a89e791459562994c121ea817d095cdeb58f8dc2
dung2k61st-hue <dung2k61st@gmail.com>
2025-11-26 09:58:32 +0700

    Update c2_2.py

--- c2_2.py
+++ c2_2.py
@@ -1,4 +1,4 @@
-print("Sinh viên: Nguyễn Nhứ Diệu")
+print("Sinh viên: TONG DOAN DAI DUNG")
 print("MSV: 245752021610124")
 
 import math
@@ -14,3 +14,4 @@ d2 = (y2 - y1) * (y2 - y1)
 res = math.sqrt(d1 + d2)
 
 print("Distance between two points:", res)
+
